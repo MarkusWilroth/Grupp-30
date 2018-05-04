5c14f8b3dc2fc9cc13b9a4abf2e58743f551104d
fobian1 <fabbepingis@gmail.com>
2018-05-04 13:08:51 +0200

    Masteriods

---
+++ Masteroids/Masteroids/Art.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids // Simon
+{
+    static class Art
+    {
+        public static Texture2D BossTex;
+        public static Texture2D BulletTex;
+        public static Texture2D AsteroidTex;
+
+        public static void Initialize(ContentManager content)
+        {
+            BossTex = content.Load<Texture2D>("boss");
+            BulletTex = content.Load<Texture2D>("laser");
+            AsteroidTex = content.Load<Texture2D>("asteroid3");
+        }
+    }
+}
---
+++ Masteroids/Masteroids/Bullet.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    class Bullet : GameObject
+    {
+        private int damage, age;
+        float bulletTimer, speed;
+        protected Vector2 center;
+        protected Vector2 origin;
+        List<Bullet> bulletList = new List<Bullet>();
+        public Rectangle bulletRec;
+        public Color[] textureData;
+        public Vector2 Direction;
+
+        public int Damage
+        {
+            get { return damage; }
+        }
+
+        public bool IsDead()
+        {
+            return age > 100;
+        }
+        
+        public Bullet(Vector2 position, float speed, int damage, Vector2 direction, Viewport viewport)
+            : base(position, viewport)
+        {
+            this.position = position;
+            this.texture = Art.BulletTex;
+            this.damage = damage;
+            this.speed = speed;
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+            center = new Vector2(this.position.X + texture.Width / 2, this.position.Y + texture.Height / 2);
+            textureData = new Color[texture.Width * texture.Height];
+            texture.GetData(textureData);
+
+            Direction = direction;
+        }
+
+        public void Kill()
+        {
+            this.age = 200;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            bulletRec = new Rectangle((int)position.X, (int)position.Y, 5, 5);
+            bulletTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            age++;
+            //center.Y += speed;
+
+
+            position += Direction * speed;
+
+            if (position.Y >= 200)
+            {
+                Kill();
+            }
+
+
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, bulletRec, Color.White, 0,
+                 origin, 1.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(texture, position, Color.Red);
+        }
+
+    }
+}
